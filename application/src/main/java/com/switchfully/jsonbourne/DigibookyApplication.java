773a5f7144ab152c03dafd41c196cf1949e0c3ef
RafaelWill <willekens.rafael@gmail.com>
2021-03-23 17:50:34 +0100

    Added Spring server test

--- application/src/main/java/com/switchfully/jsonbourne/DigibookyApplication.java
+++ application/src/main/java/com/switchfully/jsonbourne/DigibookyApplication.java
@@ -1,4 +1,4 @@
-package com.switchfully.jsonbourne.war;
+package com.switchfully.jsonbourne;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ application/src/test/java/com/switchfully/jsonbourne/DigibookyApplicationTest.java
@@ -0,0 +1,12 @@
+package com.switchfully.jsonbourne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigibookyApplicationTest {
+
+    @Test
+    void contextSpringTest() {
+    }
+}
