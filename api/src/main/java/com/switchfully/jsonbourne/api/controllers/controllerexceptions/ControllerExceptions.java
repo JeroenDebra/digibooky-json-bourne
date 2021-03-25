7362566a663a1ee44ccfaa5c90bdd2011438ecff
jedeb <jeroendebra@gmail.com>
2021-03-25 20:31:35 +0100

    changed isAdmin and isLibrarian method and changed place exception is thrown

--- api/src/main/java/com/switchfully/jsonbourne/api/controllers/controllerexceptions/ControllerExceptions.java
+++ api/src/main/java/com/switchfully/jsonbourne/api/controllers/controllerexceptions/ControllerExceptions.java
@@ -8,7 +8,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
